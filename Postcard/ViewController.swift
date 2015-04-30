1491396cfc37e68c4d69785ba1a842021340be23
fcdeveloper <alex240505@gmail.com>
2015-04-30 13:02:51 -0400

    added name field code

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,8 +11,11 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+
     @IBOutlet weak var sendMailButton: UIButton!
     
     
@@ -28,11 +31,17 @@ class ViewController: UIViewController {
 
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        //Name
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+        //Message
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
-        
         enterMessageTextField.text = ""
+        //Hide Keyboard
         enterMessageTextField.resignFirstResponder()
  
         //change the text to sendMailButton
